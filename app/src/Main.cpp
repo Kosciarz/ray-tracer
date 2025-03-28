e7df565404aafaaa17953dc2adf9222fdc4fc2d6
Kosciarz <bartoszbachul1@gmail.com>
2025-03-28 16:17:33 +0100

    link libraries; add basic window script

--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -1,9 +1,53 @@
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
+
 #include <iostream>
+#include <vector>
+
+constexpr int width = 720;
+constexpr int height = 480;
+
+void FramebufferSizeCallback(GLFWwindow* window, const int width, const int height)
+{
+	glViewport(0, 0, width, height);
+}
 
 int main()
 {
-	
-	std::cout << "Hello, cmake!" << '\n';
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPACT, GL_TRUE);
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(width, height, "window", NULL, NULL);
+	if (!window)
+	{
+		std::cerr << "Error: failed to create GLFW window" << '\n';
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, FramebufferSizeCallback);
+
+	const int version = gladLoadGL(glfwGetProcAddress);
+	if (version == 0)
+	{
+		std::cerr << "Error: failed to initialize glad" << '\n';
+		glfwTerminate();
+		return -1;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
 
