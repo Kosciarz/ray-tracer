d4a9900ec77a4efdc826f5b0e7b23fc145e441dd
Bartosz <bartoszbachul1@gmail.com>
2025-05-01 21:51:15 +0200

    Remove 2nd texture from usage

--- src/RayTracer/Application.cpp
+++ src/RayTracer/Application.cpp
@@ -41,7 +41,7 @@ namespace raytracer {
         if (window.IsErr())
             return Result<Application>::Err(window.Error());
 
-        const auto& shaderSource = ShaderSources::Load({shadersPath / "vs.glsl", shadersPath / "fs.glsl"});
+        const auto& shaderSource = ShaderSources::Load({shadersPath / "vs.vert", shadersPath / "fs.frag"});
         if (shaderSource.IsErr())
             return Result<Application>::Err(shaderSource.Error());
 
@@ -50,19 +50,17 @@ namespace raytracer {
             return Result<Application>::Err(shader.Error());
 
         auto texture1 = Texture::Create(GL_TEXTURE_2D, 0, texturesPath / "wooden_container.jpg");
-        auto texture2 = Texture::Create(GL_TEXTURE_2D, 1, texturesPath / "awesome_face.png");
 
 #pragma region buffers
 
         const std::vector<float> vertices = {
-            // positions         // texture coords
-            0.5f,  0.5f, 0.0f,   1.0f, 1.0f,   // top right
-            0.5f, -0.5f, 0.0f,   1.0f, 0.0f,   // bottom right
-           -0.5f, -0.5f, 0.0f,   0.0f, 0.0f,   // bottom left
-           -0.5f,  0.5f, 0.0f,   0.0f, 1.0f    // top left 
+            1.0f,  1.0f, 0.0f,   1.0f, 1.0f,   // top right
+            1.0f, -1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+           -1.0f, -1.0f, 0.0f,   0.0f, 0.0f,   // bottom left
+           -1.0f,  1.0f, 0.0f,   0.0f, 1.0f    // top left 
         };
 
-        const std::vector<std::uint16_t> indices = {
+        const std::vector<std::uint32_t> indices = {
             0, 1, 2,
             2, 3, 0
         };
@@ -71,12 +69,11 @@ namespace raytracer {
         vertexArray->Bind();
 
         auto vertexBuffer = VertexBuffer::Create(vertices.data(), vertices.size() * sizeof(float));
-        auto indexBuffer = IndexBuffer::Create(GL_UNSIGNED_SHORT, indices.size() * sizeof(std::uint16_t), indices.data(), GL_STATIC_DRAW);
+        auto indexBuffer = IndexBuffer::Create(indices.size() * sizeof(std::uint32_t), indices.data(), GL_STATIC_DRAW);
         vertexArray->AddIndexBuffer(indexBuffer);
 
         vertexArray->AddVertexBuffer(vertexBuffer, 0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
         vertexArray->AddVertexBuffer(vertexBuffer, 1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
-        vertexArray->Unbind();
 
 #pragma endregion
 
@@ -91,11 +88,9 @@ namespace raytracer {
         app.m_AssetManager.AddVertexArray("vertexArray1", vertexArray);
         app.m_AssetManager.AddShader("shader1", shader.Value());
         app.m_AssetManager.AddTexture("texture1", texture1);
-        app.m_AssetManager.AddTexture("texture2", texture2);
 
         app.m_AssetManager.GetShader("shader1")->Use();
         app.m_AssetManager.GetShader("shader1")->SetUniformInt("texture1", 0);
-        app.m_AssetManager.GetShader("shader1")->SetUniformInt("texture2", 1);
 
         app.m_GlfwContext = glfwContext.ValueMove();
         app.m_Running = true;
@@ -110,11 +105,9 @@ namespace raytracer {
 
             const auto& vertexArray = m_AssetManager.GetVertexArray("vertexArray1");
             const auto& shader = m_AssetManager.GetShader("shader1");
-            const auto& texture1 = m_AssetManager.GetTexture("texture1");
-            const auto& texture2 = m_AssetManager.GetTexture("texture2");
+            const auto& texture = m_AssetManager.GetTexture("texture1");
 
-            Renderer::Draw(vertexArray, shader, 
-                std::vector{texture1, texture2});
+            Renderer::Draw(vertexArray, shader, std::vector{texture});
 
             m_Window->PollEvents();
             m_Window->SwapBuffers();
