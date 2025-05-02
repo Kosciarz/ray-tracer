22b433e3e0317b9f8a0e0f64aed15a7c7ea9c83a
Bartosz <bartoszbachul1@gmail.com>
2025-05-02 23:49:55 +0200

    Add header to include all headers from the Core library

---
+++ src/RayTracer/RayTracerCore.h
@@ -0,0 +1,19 @@
+#pragma once
+
+// Main include for RayTracer application
+
+#include "RayTracerGL.h"
+
+#include "Application.h"
+#include "Window.h"
+#include "Utils.h"
+#include "Result.h"
+#include "GlfwContext.h"
+#include "AssetManager.h"
+#include "Layer.h"
+
+#include "Renderer/Image.h"
+#include "Renderer/Renderer.h"
+#include "Renderer/Shader.h"
+#include "Renderer/Buffer.h"
+#include "Renderer/VertexArray.h"
