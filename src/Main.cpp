6c3a9bb5ef79c8cf53db7d71e5065aeec4e3d43b
Bartosz <bartoszbachul1@gmail.com>
2025-05-30 23:48:08 +0200

    Update main function for better error handling

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,15 +1,16 @@
 #include <iostream>
+#include <exception>
 
 #include "App/Application.hpp"
 
-int main(int argc, char* argv[])
+int main()
 {
     try
     {
         raytracer::Application app;
         app.Run();
     }
-    catch (const std::runtime_error& e)
+    catch (const std::exception& e)
     {
         std::cerr << "Error: " << e.what() << '\n';
         return EXIT_FAILURE;
